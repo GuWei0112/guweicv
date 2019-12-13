451b9927a3d6e16e1f9ddc7cc8dffb6e7057acda
GuWei <a0983828796@gmail.com>
2019-12-13 15:47:16 +0800

    update for the first commit

---
+++ src/components/header/Header.components.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {Header} from './header.style'
+export default () => (
+  <div>
+    <Header to="education">Educations</Header>
+    <Header to="skills">Skills</Header>
+    <Header to="contact">Contact</Header>
+  </div>
+);
---
+++ src/components/header/header.style.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const Header = styled(Link)`
+  display: flex;
+  justify-content: flex-end;
+  padding: 10px 15px;
+`;
