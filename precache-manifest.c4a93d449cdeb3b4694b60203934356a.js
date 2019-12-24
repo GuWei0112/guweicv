10e9c4c06232e517bcd8b8d9729bc6a5c6e3f792
GuWei <a0983828796@gmail.com>
2019-12-24 17:42:38 +0800

    Updates

---
+++ precache-manifest.c4a93d449cdeb3b4694b60203934356a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a2d3d67626efa1626d43688d3dc28a8",
+    "url": "/guweicv/index.html"
+  },
+  {
+    "revision": "1ab7be1fd46d51c82d05",
+    "url": "/guweicv/static/css/main.36ba8075.chunk.css"
+  },
+  {
+    "revision": "c8b5a4b6229440447cfb",
+    "url": "/guweicv/static/js/2.49e84bf8.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/guweicv/static/js/2.49e84bf8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1ab7be1fd46d51c82d05",
+    "url": "/guweicv/static/js/main.0d95a319.chunk.js"
+  },
+  {
+    "revision": "3a194f22e43552d829c6",
+    "url": "/guweicv/static/js/runtime-main.efbf841d.js"
+  }
+]);
